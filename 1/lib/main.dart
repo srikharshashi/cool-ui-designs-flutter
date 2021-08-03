cf97acb5416f27b97117bf3bfc4f5debf5648bda
Srikhar Shashi D <dasojusrikhar@gmail.com>
2021-08-03 10:42:15 +0530

    Readme added

--- 1/lib/main.dart
+++ 1/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(textTheme: TextTheme()),
       title: 'Talent Hunt -- Raja Yogan',
       home: Screen1(),
